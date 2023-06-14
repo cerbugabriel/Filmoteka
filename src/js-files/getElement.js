4c49bb0684633f5b93d5f9819370f571aef0f52c
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-06-14 20:18:06 +0300

    Update getElement.js

--- src/js-files/getElement.js
+++ src/js-files/getElement.js
@@ -1,7 +1,9 @@
-export const getElement = selection => {
+const getElement = selection => {
   const element = document.querySelector(selection);
   if (element) {
     return element;
   }
   throw new Error(`the ${selection} dose not exist`);
 };
+
+export default getElement;
