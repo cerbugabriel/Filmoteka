14cc05cca1cb5bd2bb6c61dce0b1c618feb9c7fc
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-06-15 23:38:07 +0300

    done with the gallery js

--- src/js-files/setupGallery.js
+++ src/js-files/setupGallery.js
@@ -1,19 +1,53 @@
 import getElement from './getElement';
 
-const testGallery = getElement('.test-gallery');
+const moviesGallery = getElement('.gallery');
 
-export const setupGallery = data => {
+export const setupGallery = (data, movieTypes) => {
+  moviesGallery.innerHTML = '';
   const galleryContent = data
     .map(movie => {
-      return `    <li class="test-gallery__card">
-              <img
-                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                alt="${movie.title}"
-              />
-              <h2>Name : ${movie.title}</h2>
-              <h4>Release date: ${movie.release_date}</h4>
-            </li>`;
+      let {
+        genre_ids: movieType,
+        name,
+        poster_path: movieImg,
+        release_date: releaseDate,
+        id,
+      } = movie;
+      movieType = displaMovieType(movieType, movieTypes);
+      releaseDate = getRealeaseDate(releaseDate);
+      movieImg = movieImg
+        ? `https://image.tmdb.org/t/p/w500${movieImg}`
+        : `../images/no-signal-img.jpg`;
+      return `<div class="photo-card" data-id="${id}" >
+  <a href="">
+    <img src="${movieImg}" class="image" alt="${name}" />
+  </a>
+  <div class="container-info">
+    <b>${name}</b>
+    <div class="cont-descr">
+      <p class="descr-item">${movieType}</p>
+      <p class="descr-item">|</p>
+      <p class="descr-item">${releaseDate}</p>
+    </div>
+  </div>
+</div>`;
     })
     .join('');
-  testGallery.innerHTML = galleryContent;
+  moviesGallery.innerHTML = galleryContent;
 };
+
+function getRealeaseDate(realeaseDate) {
+  const year = realeaseDate ? realeaseDate.split('-')[0] : 'Unknown';
+  return year;
+}
+
+function displaMovieType(genreArray, movieTypes) {
+  let types = '';
+  movieTypes.forEach(type => {
+    if (genreArray.includes(type.id)) {
+      types = types + type.name + ', ';
+    }
+  });
+  types = types ? types.slice(0, -2) : 'Unknown';
+  return types;
+}
