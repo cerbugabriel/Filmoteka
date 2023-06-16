620f6f83d08fc738f0dcad18c2abbfdb563dba7a
CristinaAI22 <cristina.ionica22@gmail.com>
2023-06-16 14:36:06 +0300

    fisiere pt paginare

---
+++ src/js-files/pagination.js
@@ -0,0 +1,4 @@
+import { API_KEY, BASE_URL, PER_PAGE, currentPage } from './utils';
+
+//const limit = PER_PAGE;
+//currentPAge
--- src/js-files/utils.js
+++ src/js-files/utils.js
@@ -1,6 +1,7 @@
 export const API_KEY = '53b2ac0d64cbeedea763734f4fe8a4ce';
 export let currentPage = 1;
 export const BASE_URL = 'https://api.themoviedb.org/3';
+export const PER_PAGE = 9;
 
 // https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=53b2ac0d64cbeedea763734f4fe8a4ce
 // https://api.themoviedb.org/3/genre/movie/list?api_key=53b2ac0d64cbeedea763734f4fe8a4ce
