1af0608b50254631e1aa62309596266e47ba7dff
CristinaAI22 <118814335+CristinaAI22@users.noreply.github.com>
2023-06-16 17:04:09 +0000

    Deploying to gh-pages from @ CristinaAI22/Filmoteka@2c13dc1d672ebebbaed89e892e0f8e8deaa7ad0f 🚀

---
+++ index.fd7dafd3.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".image"),t=document.getElementById("film_info_modal"),n=document.querySelector(".img_content"),c=document.getElementById("close-button");e.addEventListener("click",()=>{n.innerHTML=`<img src="${e.src}">`,t.showModal()}),c.addEventListener("click",()=>{t.close()});
+//# sourceMappingURL=index.fd7dafd3.js.map
