652c772417221fc4c324cc2277bf7b10f094d2d6
CristinaAI22 <cristina.ionica22@gmail.com>
2023-06-17 10:28:34 +0300

    fisiere carusel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // imports
 import { fetchAllMovies } from './js-files/fetchMovies';
 import { handlePagination } from './js-files/pagination';
+import { fetchPopularMovies } from './js-files/carousel';
 fetchAllMovies();
 handlePagination();
+fetchPopularMovies();
