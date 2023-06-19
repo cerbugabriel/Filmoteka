9c1dd6bf016f86d6357dcca84886ff88314fbdfa
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-06-19 21:26:06 +0300

    Update createLibraryDOM.js

--- src/js-files/library/createLibraryDOM.js
+++ src/js-files/library/createLibraryDOM.js
@@ -1,3 +1,12 @@
+// imports
 import { getElement } from './library-utils';
 
 export const createLibraryDOM = moviesArr => {};
+
+function getGenres (arr){
+  let types = '';
+  arr.forEach((movie) =>{
+    types = types + ', ' + movie.name;
+  })
+  if()
+}
