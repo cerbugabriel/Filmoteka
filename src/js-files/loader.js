ffded5523acc8fc89461b8562ca94e4253661771
Radu-Gabriel <110191915+gabrielRaduDanila@users.noreply.github.com>
2023-06-20 23:35:14 +0300

    new

--- src/js-files/loader.js
+++ src/js-files/loader.js
@@ -16,3 +16,13 @@ export function showNotification() {
 export function showWarning() {
   Notiflix.Notify.warning('Oops! Something went wrong, but life goes on');
 }
+
+export function showNotificationEmtyValue() {
+  Notiflix.Notify.warning(
+    'Please enter the name of a movie to initiate a search.'
+  );
+}
+
+export function tooManyMovies() {
+  Notiflix.Notify.warning('Sooo many movies...');
+}
