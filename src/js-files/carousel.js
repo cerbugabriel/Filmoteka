8c42168f04b14ad567c8094c11e6e1da4b38ce14
CristinaAI22 <cristina.ionica22@gmail.com>
2023-06-22 23:35:03 +0300

    carusel vechi

--- src/js-files/carousel.js
+++ src/js-files/carousel.js
@@ -1,6 +1,5 @@
 import { API_KEY, BASE_URL } from './utils';
 import { showWarning } from './loader';
-import { fetchMovieTrailer } from './fetchMovieTrailer';
 
 export const fetchPopularMovies = async () => {
   try {
@@ -35,45 +34,8 @@ const displayMovies = movies => {
     image.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     image.alt = movie.title;
 
-    const watchTrailerLink = document.createElement('a');
-    watchTrailerLink.classList.add('watch-trailer-link');
-    watchTrailerLink.href = '#';
-    watchTrailerLink.dataset.movieId = movie.id;
-
-    const playIcon = createPlayIcon();
-    watchTrailerLink.innerHTML = playIcon;
-
-    movieItem.appendChild(watchTrailerLink);
     movieItem.appendChild(image);
-
     cardHolder.appendChild(movieItem);
-
-    watchTrailerLink.addEventListener('click', async event => {
-      event.preventDefault();
-
-      // const clickedMovieId = event.target.dataset.movieId;
-      const clickedMovieId = watchTrailerLink.getAttribute('data-movie-id');
-      const clickedMovie = movies.find(
-        movie => movie.id === parseInt(clickedMovieId)
-      );
-
-      if (clickedMovie.id) {
-        try {
-          const trailerKey = await fetchMovieTrailer(clickedMovie.id);
-
-          if (trailerKey) {
-            const youtubeTrailerURL = `https://www.youtube.com/watch?v=${trailerKey}`;
-
-            // Open the trailer in a new tab
-            window.open(youtubeTrailerURL, '_blank');
-          } else {
-            console.log('No trailer found for the movie.');
-          }
-        } catch (err) {
-          console.log(err);
-        }
-      }
-    });
   });
 };
 
@@ -105,7 +67,6 @@ const startCarousel = () => {
       }, 500);
     }
   };
-
   const slideItems = cardHolder.querySelectorAll('.swiper__slide');
   slideItems.forEach(slide => {
     slide.addEventListener('mouseover', () => {
@@ -121,11 +82,4 @@ const startCarousel = () => {
     currentPosition = (currentPosition + 1) % totalSlides;
     slideCardHolder();
   }, 3000);
-};
-
-const createPlayIcon = () => {
-  return `<svg class="youtube-logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="44" height="44">
-    <circle cx="12" cy="12" r="10" fill="#FF0000" />
-    <path fill="#FFFFFF" d="M9.75 7.52v8.956l6.25-4.478z" />
-  </svg>`;
 };
