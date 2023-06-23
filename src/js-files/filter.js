64b2a4d5f8532680a7ca1ca8806acd6bfc01db93
chiri03 <lena.kiriac.97@gmail.com>
2023-06-23 21:42:28 +0300

    filter and modalwindow adjustments

--- src/js-files/filter.js
+++ src/js-files/filter.js
@@ -1,4 +1,8 @@
 import { getAllMovies, getMoviesType, getFilteredMovies } from './fetchMovies';
+import { setupGallery } from './setupGallery';
+import getElement from './getElement';
+
+const moviesGallery = getElement('.gallery');
 
 const refs = {
   filterForm: document.querySelector('#filter-form'),
@@ -9,10 +13,7 @@ const refs = {
 
 export async function getFilterGenres() {
   const genres = await getMoviesType();
-
-  console.log(await getFilteredMovies());
-
-  refs.selectGenre.innerHTML = `<option value="0"> Genre </option>`;
+  refs.selectGenre.innerHTML = `<option value=""> Genre </option>`;
   genres.forEach(g => {
     refs.selectGenre.innerHTML += `<option value="${g.id}">${g.name}</option>`;
   });
@@ -20,7 +21,6 @@ export async function getFilterGenres() {
 
 export async function getMoviesYearFilter() {
   const moviesData = await getAllMovies();
-  console.log(moviesData);
   const movies = moviesData.results;
   let years = [];
 
@@ -29,32 +29,28 @@ export async function getMoviesYearFilter() {
       years.push(m.release_date.split('-')[0]);
     }
   });
-
-  console.log(years);
 }
 
-// WORK IN PROGRESS
 // filter
 
-const genre = document.getElementById('genreForm');
-const year = document.getElementById('yearForm');
-const average = document.getElementById('averageForm');
+const selects = [refs.selectGenre, refs.selectYear, refs.selectAverage];
 
-genre.addEventListener('change', e => {
-  console.log(refs.filterForm.option);
-  // console.log(refs.selectAverage.value);
-  // console.log(e.target.value);
+selects.forEach(select => {
+  select.addEventListener('change', async event => {
+    event.preventDefault();
+    const genre = refs.selectGenre.value;
+    const year = refs.selectYear.value;
+    const average = refs.selectAverage.value;
+
+    const filterObject = {
+      genre: genre,
+      year: year,
+      vote: average,
+    };
+
+    const filtredMovies = await getFilteredMovies(filterObject);
+    const movieTypes = await getMoviesType();
+
+    setupGallery(filtredMovies.results, movieTypes, moviesGallery);
+  });
 });
-//     const year = document.getElementById('yearForm');
-//     const average = document.getElementById('averageForm');
-
-// var tabs = document.querySelectorAll('#filter-form select');
-// console.log(tabs);
-// tabs.forEach(tab => {
-//   tab.addEventListener('click', () => {
-//     const genre = document.getElementById('genreForm');
-//     const year = document.getElementById('yearForm');
-//     const average = document.getElementById('averageForm');
-//     console.log(genre.value, year.value, average.value);
-//   });
-// });
